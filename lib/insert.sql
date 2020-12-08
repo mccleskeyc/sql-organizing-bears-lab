c3745497609850ecbf5557f203c95433a80c770a
mccleskeyc <mccleskeyc@gmail.com>
2020-12-07 20:55:59 -0500

    commit tree f9bf422a7de49eba42df3f064fbb17fae3095f08

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 16, "F", "Black", "Shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 100, "M", "Red", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabita", 14, "F", "Brown", "Fun", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 16, "F", "Rainbow", "Happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 24, "F", "Black", "Shy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 47, "M", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "Red", "Aging", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, "M", "Grizzly", "Murderous", 0);
